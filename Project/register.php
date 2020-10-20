00b6441c73fa2c950609cd1e9eebb1a0597a602d
jp769 <jp769@njit.edu>
2020-10-20 22:12:12 +0000

    added required maxlength 60 to password

--- Project/register.php
+++ Project/register.php
@@ -75,7 +75,7 @@ if (!isset($username)) {
         <label for="user">Username:</label>
         <input type="text" id="user" name="username" required maxlength="60" value="<?php safer_echo($username); ?>"/>
         <label for="p1">Password:</label>
-        <input type="password" id="p1" name="password" required/>
+        <input type="password" id="p1" name="password" required maxlength="60"/>
         <label for="p2">Confirm Password:</label>
         <input type="password" id="p2" name="confirm" required/>
         <input type="submit" name="register" value="Register"/>
