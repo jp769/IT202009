0eb1c81b804f8a8385734caee9451f96c3ff4b5d
jp769 <jp769@njit.edu>
2020-10-21 02:13:05 +0000

    commented out login with username

--- Project/login.php
+++ Project/login.php
@@ -3,8 +3,8 @@
 <form method="POST">
     <label for="email">Email:</label>
     <input type="email" id="email" name="email" />
-    <label for="user">Username:</label>
-    <input type="text" id="user" name="username" maxlength="60"/>
+    //<label for="user">Username:</label>
+    //<input type="text" id="user" name="username" maxlength="60"/>
     <label for="p1">Password:</label>
     <input type="password" id="p1" name="password" required/>
     <input type="submit" name="login" value="Login"/>
@@ -15,21 +15,17 @@
 if (isset($_POST["login"])) {
     $email = null;
     $password = null;
-    $username = null;
+    //$username = null;
     if (isset($_POST["email"])) {
         $email = $_POST["email"];
-    }
-    if (isset($_POST["username"])) {
-	$username = $_POST["username"];
     }
     if (isset($_POST["password"])) {
         $password = $_POST["password"];
     }
     $isValid = true;
-    if ((!isset($email) || !isset($username)) || !isset($password)) {
+    if ((!isset($email) || !isset($password)) {
         $isValid = false;
     }
-    
     if ($email && !strpos($email, "@")) {
         $isValid = false;
         echo "<br>Invalid email<br>";
