2c4543e5610e4f0cc733f36642ddccff48552abb
jp769 <jp769@njit.edu>
2020-10-21 00:55:34 +0000

    profile preloading username

--- Project/login.php
+++ Project/login.php
@@ -35,7 +35,7 @@ if (isset($_POST["login"])) {
     if ($isValid) {
         $db = getDB();
         if (isset($db)) {
-            $stmt = $db->prepare("SELECT id, email, password, username from Users WHERE email = :email LIMIT 1  username =:username");
+            $stmt = $db->prepare("SELECT id, email, password, username from Users WHERE email = :email LIMIT 1");
 
             $params = array(":email" => $email, ":username" => $username);
             $r = $stmt->execute($params);
