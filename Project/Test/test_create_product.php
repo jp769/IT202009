821085343c509b38bbae1a2e2335a7ceb472f569
jp769 <jp769@njit.edu>
2020-11-12 03:31:35 +0000

    fixing Test folder

--- Project/Test/test_create_product.php
+++ Project/Test/test_create_product.php
@@ -1,8 +1,8 @@
-<?php require_once(__DIR__ . "../partials/nav.php"); ?>
+<?php require_once(__DIR__ . "/../partials/nav.php"); ?>
 <?php
 if (!has_role("Admin")) {
 	flash("You don't have permission to access this page.");
-	die(header("Location: ../login.php"));
+	die(header("Location: /../login.php"));
 }
 ?>
 
@@ -47,4 +47,4 @@ if(isset($_POST["save"])){
 }
 
 ?>
-<?php require(__DIR__ . "../partials/flash.php");
+<?php require(__DIR__ . "/../partials/flash.php");
