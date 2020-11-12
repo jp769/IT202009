49905fdd00eff182659cdc4a9934225539dfcde3
jp769 <jp769@njit.edu>
2020-11-12 19:51:01 +0000

    cart edit

--- Project/Test/test_edit_cart.php
+++ Project/Test/test_edit_cart.php
@@ -13,65 +13,13 @@ if(isset($_GET["id"])){
 	$id = $_GET["id"];
 }
 ?>
-<?php
 //saving
-if(isset($_POST["save"])){
-	//TODO add proper validation/checks
-	$name = $_POST["name"];
-	$product = $_POST["product_id"];
-	if ($product <= 0) {
-	    $product = null;
-	}
-	$quantity = $_POST["quantity"]
-	$price = $_POST["price"]
-	$user = get_user_id();
-	if (isset($id)) {
-        $stmt = $db->prepare("UPDATE Cart set product_id=:product_id, quantity=:quantity, price=:price where id=:id");
-        $r = $stmt->execute([
-            ":product_id" => $product,
-            ":quantity" => $quantity,
-            ":price" => $price,
-            ":id" => $id
-        ]);
-        if ($r) {
-            flash("Updated successfully with id: " . $id);
-        }
-        else {
-            $e = $stmt->errorInfo();
-            flash("Error updating: " . var_export($e, true));
-        }
-    }
-    else {
-        flash("ID isn't set, we need an ID in order to update");
-    }
-}
-?>
-<?php
 //fetching
-$result = [];
-if (isset($id)) {
-    $id = $_GET["id"];
-    $db = getDB();
-    $stmt = $db->prepare("SELECT * FROM Cart where id = :id");
-    $r = $stmt->execute([":id" => $id]);
-    $result = $stmt->fetch(PDO::FETCH_ASSOC);
-}
-//get Products for dropdown
-$db = getDB();
-$stmt = $db->prepare("SELECT id,name,price from Products LIMIT 10");
-$r = $stmt->execute();
-$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
-?>
+
 
 <div class="form">
     <form method="POST">
         <label for="prod-label">ProductID</label>
-        <select name="product_id" value="<?php echo $result["product_id"];?>" >
-            <option value="-1">None</option>
-            <?php foreach ($products as $product): ?>
-                <option value="<?php safer_echo($product["id"]); ?>" <?php echo ($result["product_id"] == $product["id"] ? 'selected="selected"' : ''); ?>
-                ><?php safer_echo($product["name"]); ?></option>
-            <?php endforeach; ?>
         </select>
         <label for="prod-label">Quantity</label>
         <input type="number" min="0" name="quantity" value="<?php echo $result["quantity"]; ?>"/>
