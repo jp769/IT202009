25eb0ac33a27a4695f5f243e31664944aea1177c
jp769 <jp769@njit.edu>
2020-11-12 03:50:41 +0000

    partials nav

--- Project/partials/nav.php
+++ Project/partials/nav.php
@@ -7,12 +7,12 @@ require_once(__DIR__ . "/../lib/helpers.php");
 <ul class="nav">
     <li><a href="home.php">Home</a></li>
     <?php if (!is_logged_in()): ?>
-        <li><a href=<?php getURL("login.php");?>>Login</a></li>
-        <li><a href=<?php getURL("register.php");?>>Register</a></li>
+        <li><a href=<?php echo getURL("login.php");?>>Login</a></li>
+        <li><a href=<?php echo getURL("register.php");?>>Register</a></li>
     <?php endif; ?>
     <?php if (is_logged_in()): ?>
-        <li><a href=<?php getURL("profile.php");?>>Profile</a></li>
-        <li style="float:right"><a class="active" href=<?php getURL("logout.php");?>>Logout</a></li>
+        <li><a href=<?php echo getURL("profile.php");?>>Profile</a></li>
+        <li style="float:right"><a class="active" href=<?php echo getURL("logout.php");?>>Logout</a></li>
     <?php endif; ?>
     <?php if (has_role("Admin")): ?>
 	<div class="dropdown">
