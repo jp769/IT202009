914d97e5cec9270891974fc5f8256ec55fc2e8cc
jp769 <jp769@njit.edu>
2020-11-12 01:33:26 +0000

    testing

--- Project/Test/test_create_product.php
+++ Project/Test/test_create_product.php
@@ -1,4 +1,4 @@
-<?php require_once(__DIR__ . "/partials/nav.php");?>
+<?php require_once(__DIR__ . "/../partials/nav.php");?>
 <?php
 if (!has_role("Admin")) {
 	flash("You don't have permission to access this page.");
@@ -46,4 +46,4 @@ if(isset($_POST["save"])){
 }
 
 ?>
-<?php require(__DIR__ . "/partials/flash.php");
+<?php require(__DIR__ . "/../partials/flash.php");
