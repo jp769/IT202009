ec098de8d4c2a0a318f5db8aea02f070a8a489e6
jp769 <jp769@njit.edu>
2020-11-17 22:18:57 -0500

    navbar links fixed

--- Project/partials/nav.php
+++ Project/partials/nav.php
@@ -7,6 +7,7 @@ require_once(__DIR__ . "/../lib/helpers.php");
 <ul class="nav">
     <li><a href=<?php echo getURL("home.php");?>>Home</a></li>
     <?php if (!is_logged_in()): ?>
+        <li><a href=<?php echo getURL("product_list.php");?>>Products</a></li>
         <li><a href=<?php echo getURL("login.php");?>>Login</a></li>
         <li><a href=<?php echo getURL("register.php");?>>Register</a></li>
     <?php endif; ?>
@@ -16,14 +17,14 @@ require_once(__DIR__ . "/../lib/helpers.php");
     <?php endif; ?>
     <?php if (has_role("Admin")): ?>
 	<div class="dropdown">
-	<button style="float:right" class="dropbtn">Products<i class="fa fa-caret-down"></i></button>
+	<button style="float:right" class="dropbtn">Products Test<i class="fa fa-caret-down"></i></button>
 	<div class="dropdown-content">
 	<a class="active" href=<?php echo getURL("Test/test_create_product.php");?>>Create Product</a>
 	<a class="active" href=<?php echo getURL("Test/test_list_product.php");?>>List Product</a>
 	</div>
 	</div>
 	<div class="dropdown">
-	<button style="float:right" class="dropbtn">Cart<i class="fa fa-caret-down"></i></button>
+	<button style="float:right" class="dropbtn">Cart Test<i class="fa fa-caret-down"></i></button>
 	<div class="dropdown-content">
 	<a class="active" href=<?php echo getURL("Test/test_create_cart.php");?>>Create Cart</a>
 	<a class="active" href=<?php echo getURL("Test/test_list_cart.php");?>>List Cart</a>
