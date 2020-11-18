794129cb65cdae25270fe1a91d2d960de7e65df9
jp769 <jp769@njit.edu>
2020-11-17 22:20:57 -0500

    fixed navbar public links

--- Project/partials/nav.php
+++ Project/partials/nav.php
@@ -6,8 +6,8 @@ require_once(__DIR__ . "/../lib/helpers.php");
 <nav>
 <ul class="nav">
     <li><a href=<?php echo getURL("home.php");?>>Home</a></li>
+    <li><a href=<?php echo getURL("product_list.php");?>>Products</a></li>
     <?php if (!is_logged_in()): ?>
-        <li><a href=<?php echo getURL("product_list.php");?>>Products</a></li>
         <li><a href=<?php echo getURL("login.php");?>>Login</a></li>
         <li><a href=<?php echo getURL("register.php");?>>Register</a></li>
     <?php endif; ?>
