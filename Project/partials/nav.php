558bb41b03c79deb9fe8bdb082f5b5434fd9f94a
jp769 <jp769@njit.edu>
2020-11-23 23:52:11 -0500

    cart page

--- Project/partials/nav.php
+++ Project/partials/nav.php
@@ -4,33 +4,34 @@ require_once(__DIR__ . "/../lib/helpers.php");
 ?>
 <link rel="stylesheet" href=<?php echo getURL("static/css/styles.css");?>>
 <nav>
-<ul class="nav">
-    <li><a href=<?php echo getURL("home.php");?>>Home</a></li>
-    <li><a href=<?php echo getURL("product_list.php");?>>Products</a></li>
-    <?php if (!is_logged_in()): ?>
-        <li><a href=<?php echo getURL("login.php");?>>Login</a></li>
-        <li><a href=<?php echo getURL("register.php");?>>Register</a></li>
-    <?php endif; ?>
-    <?php if (is_logged_in()): ?>
-        <li><a href=<?php echo getURL("profile.php");?>>Profile</a></li>
-        <li style="float:right"><a class="active" href=<?php echo getURL("logout.php");?>>Logout</a></li>
-    <?php endif; ?>
-    <?php if (has_role("Admin")): ?>
-	<div class="dropdown">
-	<button style="float:right" class="dropbtn">Products Test<i class="fa fa-caret-down"></i></button>
-	<div class="dropdown-content">
-	<a class="active" href=<?php echo getURL("Test/test_create_product.php");?>>Create Product</a>
-	<a class="active" href=<?php echo getURL("Test/test_list_product.php");?>>List Product</a>
-	</div>
-	</div>
-	<div class="dropdown">
-	<button style="float:right" class="dropbtn">Cart Test<i class="fa fa-caret-down"></i></button>
-	<div class="dropdown-content">
-	<a class="active" href=<?php echo getURL("Test/test_create_cart.php");?>>Create Cart</a>
-	<a class="active" href=<?php echo getURL("Test/test_list_cart.php");?>>List Cart</a>
-	</div>
-	</div>
-    <?php endif; ?>
+    <ul class="nav">
+        <li><a href=<?php echo getURL("home.php");?>>Home</a></li>
+        <?php if (!is_logged_in()): ?>
+            <li><a href=<?php echo getURL("login.php");?>>Login</a></li>
+            <li><a href=<?php echo getURL("register.php");?>>Register</a></li>
+        <?php endif; ?>
+        <?php if (is_logged_in()): ?>
+            <li><a href=<?php echo getURL("profile.php");?>>Profile</a></li>
+            <li><a href=<?php echo getURL("cart.php");?>>Cart</a></li>
+            <li style="float:right"><a class="active" href=<?php echo getURL("logout.php");?>>Logout</a></li>
+        <?php endif; ?>
+        <?php if (has_role("Admin")): ?>
+            <div class="dropdown">
+                <button style="float:right" class="dropbtn">/Test<i class="fa fa-caret-down"></i></button>
+                <div class="dropdown-content">
+                    <a class="active" href=<?php echo getURL("Test/test_create_product.php");?>>Create Product</a>
+                    <a class="active" href=<?php echo getURL("Test/test_list_product.php");?>>List Product</a>
+                    <a class="active" href=<?php echo getURL("Test/test_create_cart.php");?>>Create Cart</a>
+                    <a class="active" href=<?php echo getURL("Test/test_list_cart.php");?>>List Cart</a>
+                </div>
+            </div>
+            <!--    <div>-->
+            <!--        <button style="float:right" class="dropbtn">ADMIN<i class="fa fa-caret-down"></i></button>-->
+            <!--        <div class="dropdown-content">-->
+            <!--            <a class="active" href="create_product.php")>Create Product</a>-->
+            <!--        </div>-->
+            <!--	</div>-->
+        <?php endif; ?>
 
-</ul>
+    </ul>
 </nav>
