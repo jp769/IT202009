36577fe9d04772c2de0efc9abbcd24fb29e8b30c
jp769 <jp769@njit.edu>
2020-11-24 20:57:47 -0500

    click button to add to cart

--- Project/product_list.php
+++ Project/product_list.php
@@ -12,7 +12,7 @@ if (isset($_POST["query"])) {
 }
 if (isset($_POST["search"]) && !empty($query)) {
     $db = getDB();
-
+    safer_echo($sort);
     $stmt = $db->prepare("SELECT id,name,quantity,price,description,user_id,visibility,category from Products WHERE name like :q or category like :q  LIMIT 10");
     $r = $stmt->execute([":q" => "%$query%"]);
 
@@ -24,6 +24,30 @@ if (isset($_POST["search"]) && !empty($query)) {
     }
 }
 ?>
+    <script>
+        function addToCart(itemId, cost){
+
+            //https://www.w3schools.com/xml/ajax_xmlhttprequest_send.asp
+            let xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    let json = JSON.parse(this.responseText);
+                    if (json) {
+                        if (json.status == 200) {
+                            alert(json.message);
+                        } else {
+                            alert(json.error);
+                        }
+                    }
+                }
+            };
+            xhttp.open("POST", "<?php echo getURL("add_to_cart.php");?>", true);
+            //this is required for post ajax calls to submit it as a form
+            xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            //map any key/value data similar to query params
+            xhttp.send("itemId="+itemId);
+        }
+    </script>
 
     <form method="POST">
         <input name="query" placeholder="Search" value="<?php safer_echo($query); ?>"/>
@@ -72,6 +96,12 @@ if (isset($_POST["search"]) && !empty($query)) {
                             <?php endif;?>
                             <a type="button" href="<?php echo getURL("product_view.php");?>?id=<?php safer_echo($r['id']); ?>">View</a>
                         </div><br>
+
+                        <?php if (is_logged_in()): ?>
+                            <div>
+                                <button type="button" onclick="addToCart(<?php echo $r["id"];?>,<?php echo $r["price"];?>);" class="btn btn-primary btn-lg">Add to Cart</button>
+                            </div>
+                        <?php endif;?>
                     </div>
                 <?php endif;?>
                 <?php endforeach; ?>
