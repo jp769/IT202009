e33bad5956a5f56917d06a8f27bd55075a8fc3f1
jp769 <jp769@njit.edu>
2020-11-24 22:38:22 -0500

    sort result by price

--- Project/product_list.php
+++ Project/product_list.php
@@ -7,14 +7,21 @@ $results = [];
 if (isset($_POST["query"])) {
     $query = $_POST["query"];
 
-    if(isset($_POST["price_sort"]))
+    if(isset($_POST["price_sort"])) {
         $sort = $_POST["price_sort"];
+        safer_echo($sort);
+    }
 }
 if (isset($_POST["search"]) && !empty($query)) {
     $db = getDB();
-    safer_echo($sort);
-    $stmt = $db->prepare("SELECT id,name,quantity,price,description,user_id,visibility,category from Products WHERE name like :q or category like :q  LIMIT 10");
-    $r = $stmt->execute([":q" => "%$query%"]);
+    if($sort == "ASC"){
+        $stmt = $db->prepare("SELECT id,name,quantity,price,description,user_id,visibility,category from Products WHERE name like :q or category like :q ORDER BY price ASC LIMIT 10");
+        $r = $stmt->execute([":q" => "%$query%"]);
+    }
+    else {
+        $stmt = $db->prepare("SELECT id,name,quantity,price,description,user_id,visibility,category from Products WHERE name like :q or category like :q  LIMIT 10");
+        $r = $stmt->execute([":q" => "%$query%"]);
+    }
 
     if ($r) {
         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
