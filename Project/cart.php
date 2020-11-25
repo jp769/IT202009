37a96d6c3fbf6f9d0a7f02d3935d0fef8379a3b9
jp769 <jp769@njit.edu>
2020-11-24 23:12:48 -0500

    delete item from cart

--- Project/cart.php
+++ Project/cart.php
@@ -10,19 +10,32 @@ if (!is_logged_in()) {
 
 ?>
 <?php
-//fetching
-$result = [];
-if (is_logged_in()) {
-    $db = getDB();
-    $user_id = $_SESSION["user"]["id"];
-    $stmt = $db->prepare("SELECT cart.id, cart.quantity, cart.price, cart.product_id, cart.user_id,Product.name as prod FROM Cart as cart JOIN Users on cart.user_id = Users.id LEFT JOIN Products Product on Product.id = cart.product_id where cart.user_id = :user_id");
-    $r = $stmt->execute([":user_id" => $user_id]);
-    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    if (!$result) {
-        $e = $stmt->errorInfo();
-        flash($e[2]);
+
+$db = getDB();
+
+if(isset($_POST["delete"])){
+    $stmt = $db->prepare("DELETE FROM Cart where id = :id");
+    $r = $stmt->execute([":id"=>$_POST["cartId"]]);
+    //fix for example bug
+    //$stmt = $db->prepare("DELETE FROM F20_Cart where id = :id AND user_id = :uid");
+    //$r = $stmt->execute([":id"=>$_POST["cartId"], ":uid"=>get_user_id()]);
+    if($r){
+        flash("Deleted item from cart", "success");
     }
 }
+
+//fetching
+$result = [];
+
+$user_id = $_SESSION["user"]["id"];
+$stmt = $db->prepare("SELECT cart.id, cart.quantity, cart.price, (cart.quantity * cart.price) as subtotal, cart.product_id, cart.user_id,Product.name as prod FROM Cart as cart LEFT JOIN Products Product on Product.id = cart.product_id where cart.user_id = :user_id");
+$r = $stmt->execute([":user_id" => $user_id]);
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if (!$result) {
+    $e = $stmt->errorInfo();
+    flash($e[2]);
+}
+
 ?>
 <h3>Cart</h3>
 <?php if (count($result) > 0): ?>
@@ -31,16 +44,27 @@ if (is_logged_in()) {
     <div class="card">
         <div class="card-title">
         </div>
+        <form method="POST">
         <div class="card-body">
             <div>
                 <p> - Info - </p>
                 <div>Name:<a href=<?php echo getURL("product_view.php");?>?id=<?php safer_echo($r['product_id']); ?>> <?php safer_echo($r["prod"]);?></a></div>
-                <div>Quantity: <?php safer_echo($r["quantity"]); ?></div>
+                <div><input type = "number" min="0" name="quantity" value="<?php safer_echo($r["quantity"]); ?>"/>
+                    <input type="hidden" name="cartID" value="<?php echo $r["id"];?>"/>
+                </div>
                 <div>Price: <?php safer_echo($r["price"]); ?></div>
-                <div>Subtotal: <?php echo ($r["quantity"] * $r["price"]); ?></div>
+                <div>Subtotal: <?php echo ($r["subtotal"]); ?></div>
                 <?php $total += ($r['quantity'] * $r['price']); ?>
             </div>
         </div>
+            <input type="submit" class="btn btn-success" name="update" value="Update">
+        </form>
+        <div>
+            <form method="POST">
+            <input type="hidden" name="cartID" value="<?php safer_echo($r["id"]);?>"/>
+            <input type="submit" class="btn btn-danger" name="delete" value="Delete Item"/>
+            </form>
+        </div>>
     </div>
     <?php endforeach; ?>
     <div class="Total">
