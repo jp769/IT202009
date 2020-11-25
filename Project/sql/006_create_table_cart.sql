303c49a5416ebaf668d8d09dfa48cbc2fd77a7cc
jp769 <jp769@njit.edu>
2020-11-24 22:15:25 -0500

    recreate cart table

--- Project/sql/006_create_table_cart.sql
+++ Project/sql/006_create_table_cart.sql
@@ -8,7 +8,7 @@ CREATE TABLE Cart
     created    datetime       default current_timestamp,
     modified   datetime       default current_timestamp on update current_timestamp,
     primary key (id),
-    foreign key (product_id) references Products (id),
-    foreign key (user_id) references Users (id),
+    foreign key (product_id) references Products (id) ON DELETE CASCADE ,
+    foreign key (user_id) references Users (id) ON DELETE CASCADE ,
     UNIQUE KEY (product_id, user_id)
 )
--- Project/sql/008_alter_cart_table.sql
+++ Project/sql/008_alter_cart_table.sql
@@ -1,8 +1,1 @@
-ALTER TABLE Cart
-    DROP FOREIGN KEY (product_id),
-    DROP FOREIGN KEY (user_id);
 
-ALTER TABLE Cart
-    ADD FOREIGN KEY (product_id) references Products (id) ON DELETE CASCADE,
-    ADD FOREIGN KEY (user_id references Users (id) ON DELETE CASCADE,
-    UNIQUE KEY (product_id, user_id)
