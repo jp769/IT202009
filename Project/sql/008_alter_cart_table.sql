32ab4461d03aa736f5efdb0779c000e643375955
jp769 <jp769@njit.edu>
2020-11-24 22:10:50 -0500

    altering cart table

--- Project/sql/008_alter_cart_table.sql
+++ Project/sql/008_alter_cart_table.sql
@@ -1,3 +1,8 @@
 ALTER TABLE Cart
-    foreign key (product_id) references Products (id) on delete cascade ,
-    foreign key (user_id) references Users (id) on delete cascade
+    DROP FOREIGN KEY (product_id),
+    DROP FOREIGN KEY (user_id);
+
+ALTER TABLE Cart
+    ADD FOREIGN KEY (product_id) references Products (id) ON DELETE CASCADE,
+    ADD FOREIGN KEY (user_id references Users (id) ON DELETE CASCADE,
+    UNIQUE KEY (product_id, user_id)
