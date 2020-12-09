34e26b5371abf5efbbed15bed2dcf05540b6a0e3
jp769 <jp769@njit.edu>
2020-12-09 16:35:04 -0500

    testing if quantity over change quantity

--- Project/checkout.php
+++ Project/checkout.php
@@ -51,8 +51,8 @@ if (!$result) {
                     <div>Subtotal: <?php echo ($r["subtotal"]); ?></div>
                     <?php $total += ($r['quantity'] * $r['price']); ?>
                 </div>
-                <?php if($r['prodQuantity'] > $r['quantity']): ?>
-                    <div>Over Quantity: <?php echo $r['prodQuantity'];?></div>
+                <?php if($r['prodQuantity'] < $r['quantity']): ?>
+                    <div>Current Stock is: <?php echo $r['prodQuantity'];?> Please change quantity</div>
                     <input type="submit" class="btn btn-success" name="update" value="Update">
                 <?php endif; ?>
             </div>
