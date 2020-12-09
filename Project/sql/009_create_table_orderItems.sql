40df4b6dd8e03d766c7175efe0dedc44f5229545
jp769 <jp769@njit.edu>
2020-12-08 23:20:29 -0500

    fixing fail to create

--- Project/sql/009_create_table_orderItems.sql
+++ Project/sql/009_create_table_orderItems.sql
@@ -8,6 +8,5 @@ CREATE TABLE IF NOT EXISTS OrderItems
     primary key (id),
     foreign key (product_id) references Products (id) ON DELETE CASCADE ,
     foreign key (order_id) references Orders (id) ON DELETE CASCADE ,
-    foreign key (quantity) references Cart (quantity) ON DELETE CASCADE
-
+    foreign key (quantity) references `Cart` (quantity) ON DELETE CASCADE
 )
