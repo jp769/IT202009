712b0cfdfe497093b0a69e3125ab1a46f94776fa
jp769 <jp769@njit.edu>
2020-12-09 21:15:24 -0500

    inseerting orders table

--- Project/api/purchase_cart.php
+++ Project/api/purchase_cart.php
@@ -6,39 +6,46 @@ if (!is_logged_in()) {
 }
 
 if(isset($_POST["userID"])){
+    if(isset($_POST["total"]))
+        $total = (int)$_POST["total"];
+
     $userID = (int)$_POST["userID"];
     $db = getDB();
-    $stmt = $db->prepare("SELECT * from Cart where user_id = :id");
-    $stmt->execute([":id"=>$userID]);
+    //
+    $stmt = $db->prepare("INSERT INTO Orders (user_id, total_price, address, payment_method) VALUES (:user_id, :total_price, :address, :payment_method)");
+    $r = $stmt->execute([":user_id"=>get_user_id(), ":total_price"=>$total, ":address",":payment_method"]);
+    //
+    $stmt = $db->prepare("SELECT * from Cart where user_id = :user_id");
+    $stmt->execute([":user_id"=>$userID]);
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
     if($result) {
         $name = $result["name"];
         $price = $result["price"];
         $quantity = 1;
         $stmt = $db->prepare("INSERT INTO Cart (user_id, product_id, price, quantity) VALUES(:user_id, :product_id, :price, :quantity) ON DUPLICATE KEY UPDATE quantity = quantity + 1, price = :price");
-        $r = $stmt->execute([":user_id"=>get_user_id(), ":product_id"=>$itemId, ":price"=>$price, ":quantity"=>1]);
+        $r = $stmt->execute([":user_id"=>get_user_id(), ":total_price"=>$total, ":quantity"=>$quantity]);
         if ($r) {
-            $response = ["status" => 200, "message" => "Added $name to cart"];
+            $response = ["status" => 200, "message" => "Purchased cart"];
             echo json_encode($response);
-            die();
+            die(header("Location: purchase_history.php"));
         }
         else{
-            $response = ["status" => 400, "error" => "There was an error adding $name to cart"];
+            $response = ["status" => 400, "error" => "There was an error putting in your order"];
             echo json_encode($response);
-            die();
+            die(header("Location: cart.php"));
         }
     }
     else{
-        $response = ["status" => 404, "error" => "Item $itemId not found"];
+        $response = ["status" => 404, "error" => "Your cart was not found"];
         echo json_encode($response);
-        die();
+        die(header("Location: home.php"));
     }
 }
 
 else{
     $response = ["status" => 400, "error" => "An unexpected error occurred, please try again"];
     echo json_encode($response);
-    die();
+    die(header("Location: cart.php"));
 }
 
 ?>
--- Project/checkout.php
+++ Project/checkout.php
@@ -35,7 +35,7 @@ if (!$result) {
 ?>
 
     <script>
-        function purchaseCart(userID, cost){
+        function purchaseCart(userID, total, address, payment){
 
             let xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function () {
@@ -54,7 +54,7 @@ if (!$result) {
             //this is required for post ajax calls to submit it as a form
             xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
             //map any key/value data similar to query params
-            xhttp.send("userID="+userID);
+            xhttp.send("userID="+userID +"&"+ "total="+total + "&"+ "address="+address + "&" + "payment="+payment);
         }
     </script>
 
@@ -100,19 +100,21 @@ if (!$result) {
 <div style="align-content: space-evenly">
     <form id="address">
     <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
-    <input type="text" id="adr" name="address" placeholder="123 Example Street">
+    <input type="text" id="adr" name="address" placeholder="123 Example Street" required>
     <label for="city"><i class="fa fa-institution"></i> City</label>
-    <input type="text" id="city" name="city" placeholder="New York">
+    <input type="text" id="city" name="city" placeholder="New York" required>
 
     <label for="state">State</label>
-    <input type="text" id="state" name="state" placeholder="NY" maxlength="2">
+    <input type="text" id="state" name="state" placeholder="NY" maxlength="2" required>
     <label for="zip">Zip</label>
-    <input type="number" id="zip" name="zip" placeholder="10001" maxlength="5">
+    <input type="number" id="zip" name="zip" placeholder="10001" maxlength="5" required>
     <br>
     <label for="payMethod">Payment Method</label>
-    <input type="text" id="payMethod" name="payMethod" placeholder="Cash">
+    <input type="text" id="payMethod" name="payMethod" placeholder="Cash" required>
     </form>
 </div>
-    <button type="button" onclick="purchaseCart(<?php echo $r["user_id"];?>,<?php echo $total;?>);" class="btn btn-primary btn-lg">Checkout</button>
+    <?php $address = $_POST["adr"] + " " + $_POST["city"] +","+$_POST["state"] + " " + $_POST["zip"]; ?>
+    <?php $payment = $_POST["payMethod"];?>
+    <button type="button" onclick="purchaseCart(<?php echo $r["user_id"];?>,<?php echo $total;?>, <?php safer_echo($address);?>, <?php safer_echo($payment);?>);" class="btn btn-primary btn-lg">Checkout</button>
 <!--    <input type="submit" class="btn btn-success" name="checkout" value="Checkout">-->
 <?php endif; ?>
