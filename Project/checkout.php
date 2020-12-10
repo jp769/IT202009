1172011af8f2c11722ae893202336ab6eaa6ecaa
jp769 <jp769@njit.edu>
2020-12-09 21:43:46 -0500

    checking address before continuing to add to table

--- Project/checkout.php
+++ Project/checkout.php
@@ -113,8 +113,24 @@ if (!$result) {
     <input type="text" id="payMethod" name="payMethod" placeholder="Cash" required>
     </form>
 </div>
-    <?php $address = $_POST["adr"] + " " + $_POST["city"] +","+$_POST["state"] + " " + $_POST["zip"]; ?>
-    <?php $payment = $_POST["payMethod"];?>
+<div>
+    <input type="submit" class="btn btn-success" name="address" value="Enter Address">
+</div>
+<?php
+$isValid = false;
+    if (isset($_POST["address"])) {
+        if(isset($_POST["adr"])and isset($_POST["city"]) and isset($_POST["state"]) and isset($_POST["zip"]) and isset($_POST["payMethod"])){
+            $isValid = true;
+            $address = $_POST["adr"] + " " + $_POST["city"] +","+$_POST["state"] + " " + $_POST["zip"];
+            $payment = $_POST["payMethod"];
+        }
+        else{
+            flash("Unable to add address, try again");
+        }
+    }
+
+?>
+    <?php if ($isValid);?>
     <button type="button" onclick="purchaseCart(<?php echo $r["user_id"];?>,<?php echo $total;?>, <?php safer_echo($address);?>, <?php safer_echo($payment);?>);" class="btn btn-primary btn-lg">Checkout</button>
 <!--    <input type="submit" class="btn btn-success" name="checkout" value="Checkout">-->
-<?php endif; ?>
+    <?php endif;?>
