8f8a70627ae6c1fd1ae63deff4723347abdf184f
jp769 <jp769@njit.edu>
2020-12-10 23:41:07 -0500

    fixing last page

--- Project/confirm.php
+++ Project/confirm.php
@@ -18,7 +18,7 @@ if(isset($_GET["id"])){
 if(isset($id)){
     $o_id = $_GET["id"];
     $db = getDB();
-    $stmt = $db->prepare("SELECT * FROM OrderItems where order_id = :order_id");
+    $stmt = $db->prepare("SELECT o.product_id, o.quantity, o.unit_price, Product.name as prod FROM OrderItems as o LEFT JOIN Products Product on Product.id = o.product_id where order_id = :order_id");
     $r = $stmt->execute([":order_id"=>$o_id]);
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
     if (!$result) {
@@ -30,6 +30,30 @@ if(isset($id)){
 }
 ?>
 
+<?php if (count($result) > 0): ?>
+    <?php $total = 0; ?>
+    <?php foreach ($result as $r): ?>
+        <div class="card">
+            <div class="card-title"></div>
+            <form method="POST">
+                <div class="card-body">
+                    <div>
+                        <p> - Info - </p>
+                        <div>Name: <?php safer_echo($r["prod"]);?> </div>
+                        <div><?php safer_echo($r["quantity"]); ?> </div>
+                        <div>Unit Price: <?php safer_echo($r["unit_price"]); ?></div>
+
+                        <?php $total += ($r['quantity'] * $r['unit_price']); ?>
+                    </div>
+                </div>
+            </form>
+        </div>
+    <?php endforeach; ?>
+    <div class="Total">
+        <div> Total: <?php safer_echo($total); ?> </div>
+    </div>
+
+<?php endif; ?>
 
 
 <?php require(__DIR__ . "/partials/flash.php");
