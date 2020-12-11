383cc19c90ff61499e467368af652081a618bff1
jp769 <jp769@njit.edu>
2020-12-10 23:30:48 -0500

    fixing last page

--- Project/confirm.php
+++ Project/confirm.php
@@ -1,10 +1,22 @@
+<?php require_once(__DIR__ . "/partials/nav.php");
+
+if (!is_logged_in()) {
+    //this will redirect to login and kill the rest of this script (prevent it from executing)
+    flash("You must be logged in to access this page");
+    die(header("Location: login.php"));
+}
+?>
+
 <?php
 if(isset($_GET["id"])){
     $o_id = $_GET["id"];
     $u = get_user_id();
+    safer_echo($o_id);
+    
 }
 
 $result = [];
+
 if(isset($id)){
     $o_id = $_GET["id"];
     $db = getDB();
@@ -18,30 +30,6 @@ if(isset($id)){
 }
 ?>
 
-<?php if (count($result) > 0): ?>
-    <?php $total = 0; ?>
-    <?php foreach ($result as $r): ?>
-        <div class="card">
-            <div class="card-title"></div>
-            <form method="POST">
-                <div class="card-body">
-                    <div>
-                        <p> - Info - </p>
-                        <div>Name: <?php safer_echo($r["prod"]);?> </div>
-                        <div><?php safer_echo($r["quantity"]); ?> </div>
-                        <div>Unit Price: <?php safer_echo($r["unit_price"]); ?></div>
-
-                        <?php $total += ($r['quantity'] * $r['unit_price']); ?>
-                    </div>
-                </div>
-            </form>
-        </div>
-    <?php endforeach; ?>
-    <div class="Total">
-        <div> Total: <?php safer_echo($total); ?> </div>
-    </div>
-
-<?php endif; ?>
 
 
 <?php require(__DIR__ . "/partials/flash.php");
