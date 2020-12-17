32d8e6f56eb40614d2be2a8c3f50816821c8ee44
jp769 <jp769@njit.edu>
2020-12-17 09:20:58 -0500

    bootstrap

--- Project/partials/nav.php
+++ Project/partials/nav.php
@@ -3,6 +3,9 @@
 require_once(__DIR__ . "/../lib/helpers.php");
 ?>
 <link rel="stylesheet" href=<?php echo getURL("static/css/styles.css");?>>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+      integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
         integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
