86d92351ebc10942fe9216d9715646d60f6c9aae
jp769 <jp769@njit.edu>
2020-12-17 23:34:08 -0500

    fixed restriction on page to logged in users

--- Project/rating.php
+++ Project/rating.php
@@ -1,6 +1,6 @@
 <?php require_once(__DIR__ . "/partials/nav.php");
 
-if (is_logged_in()) {
+if (!is_logged_in()) {
     //this will redirect to login and kill the rest of this script (prevent it from executing)
     flash("You must log in to access this page");
     die(header("Location: login.php"));
